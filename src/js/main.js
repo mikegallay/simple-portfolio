e3752383983526cac6d1fdca485db69a9ac96189
Jonathan Kelly <Jonathan.Kelly@mcgarrybowen.com>
2016-04-04 14:26:26 -0400

    latest animation classes

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,25 @@
 $(function() {
   
+  (function(){
+	  $(".cultureTile").each(function(index){
+		  if(index%2 == 0) {
+			if($(this).children("a").length > 0){
+			  $(this).addClass('pushRight');
+		  	}
+		  }
+  		});
+	})();
+  
   $("#clientToggle").on('click', function(){
 	  $('#clientList').toggle();
   });
   
+  
   $(".cultureTile").on('click', function(){
-	  if($(this).children('a').length !== 0){
-		  $(this).toggleClass('growBasis');
-	  }
-  });
+   	  if($(this).hasClass('pushRight')){
+		  $(this).next().toggleClass("shrinkMe");
+   		  $(this).toggleClass('stretchOut');
+   	  }
+   });
   
 });
