9289c4e5f26679875f09c591720b53332b2442ad
Jonathan Kelly <Jonathan.Kelly@mcgarrybowen.com>
2016-04-04 09:57:28 -0400

    Updating files

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,13 @@
 $(function() {
-  console.log('main loaded HOORAY');
+  
+  $("#clientToggle").on('click', function(){
+	  $('#clientList').toggle();
+  });
+  
+  $(".cultureTile").on('click', function(){
+	  if($(this).children('a').length !== 0){
+		  $(this).toggleClass('growBasis');
+	  }
+  });
+  
 });
