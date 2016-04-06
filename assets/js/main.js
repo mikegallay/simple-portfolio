8f1b4e8c159017fb5a0c233e57d2eae3c2686ad4
Jonathan Kelly <Jonathan.Kelly@mcgarrybowen.com>
2016-04-06 17:29:49 -0400

    Updated header with logo and added svg clock elements

--- assets/js/main.js
+++ assets/js/main.js
@@ -1616,6 +1616,7 @@ $(function() {
   		});
 	})();
   
+  
 	$("#clientToggle").on('click', function(){
 		$('#clientList').toggle();
 	});
@@ -1660,88 +1661,102 @@ $(function() {
 			 $(this).toggleClass('stretchOut');		
 		}
 	});
+	
 
+	/* animate header logo */
+	
+   	var tt = TweenMax.to;
+ 	var ts = TweenMax.set;
+	ts(rect,{rotation:-90,transformOrigin:"50% 50%"})
 
+	var l = document.getElementById('mbLogo')
+	l.addEventListener('mouseover',function(){
+		showLogo()
+	})
 
-	// $(".officeTile").on("click", function(){
-	// 	var content = $(this).children("span").html();
-	//
-	// 	$("#officeDetails").hide().html("");
-	// 	$("#officeDetails").html(content).delay(1000).show();
-	// });
+	l.addEventListener('mouseout',function(){
+		hideLogo()
+	})
 
-  	setInterval(function(){
+	function showLogo(){
+		tt(rect,.4,{transformOrigin:"50% 50%", drawSVG:"0%",overwrite:true});
+		tt(bowenTXT,.4,{x:102, ease:Quad.easeIn, delay:0, overwrite:true});
+		tt(cgarryContainer,.4,{width:102, ease:Quad.easeIn, delay:0, overwrite:true});
+		tt(owenContainer,.4,{width:79, ease:Quad.easeOut, delay:.35, overwrite:true});
+	}
+
+	function hideLogo(){
+		tt(rect,.3,{transformOrigin:"50% 50%", delay:.2, drawSVG:"100%",overwrite:true});
+		tt(bowenTXT,.3,{x:0, ease:Quad.easeIn, delay:0, overwrite:true});
+		tt(cgarryContainer,.3,{width:0, ease:Quad.easeIn, delay:0, overwrite:true});
+		tt(owenContainer,.3,{width:0, ease:Quad.easeOut, delay:0, overwrite:true});
+	}
+	
+	
+
+	
+
+	/* calculate correct times for each clock */
+	
+	/* animate time dials*/
+	ts(".clockHours", {rotation: -90, transformOrigin:"50% 50%", drawSVG: "0%", overwrite:true});
+	ts(".clockMinutes", {rotation: -90, transformOrigin:"50% 50%", drawSVG: "0%", overwrite:true});
+	
+		
+	// Initialize clocks
+	(function(){
 		$(".officeTile").each(function(index){
-			var timeOffset = parseInt($(this).data("timeOffset"));
-
-		  	var d = new Date();
-
-			var utc = d.getTime() + (d.getTimezoneOffset() * 60000);
-
-			var nd = new Date(utc + (3600000*timeOffset));
-  
+			var timeOffset = parseInt($(this).data("timeOffset")),
+		  	d = new Date(),
+			utc = d.getTime() + (d.getTimezoneOffset() * 60000),
+			nd = new Date(utc + (3600000*timeOffset));
+  		  	
+			var ndHours = nd.getHours();
+			if(ndHours > 12) {
+				ndHours -= 12;
+			}
+			
+			var clockHours = ((ndHours/12) * 100) + "%";
+			var clockMinutes = (100 * (nd.getMinutes()/60))+"%";
+			
 			nd = nd.toLocaleString({hour: 'numeric', minute: 'numeric'}).replace(/:\d{2}\s/,' ').split(",")[1];
-  
+  		  	
 			$(this).children("a").find("time").text(nd);
-		});  		
-  	}, 1000);
-
-
-	var loader = document.getElementsByClassName('loader')
-	, border = document.getElementsByClassName('border')
-	, α = 0
-	, π = Math.PI
-	, t = 30;
-
-	(function draw() {
-		α++;
-		α %= 360;
-		var r = ( α * π / 180 )
-		  , x = Math.sin( r ) * 150
-		  , y = Math.cos( r ) * - 150
-		  , mid = ( α > 180 ) ? 1 : 0
-		  , anim = 'M 0 -150 A 150 150 1 ' 
-		         + mid + ' 1 ' 
-		         +  x  + ' ' 
-		         +  y;//  + ' z';
-		//[x,y].forEach(function( d ){
-		//  d = Math.round( d * 1e3 ) / 1e3;
-		//});
-		
-		for(var i = 0; i < loader.length; i++) {
-			loader[i].setAttribute( 'd', anim );
-			border[i].setAttribute( 'd', anim );
-		}
-
-		setTimeout(draw, t); // Redraw
-		})();
-		
-		/* animate header logo */
-		
-	   	var tt = TweenMax.to;
-	 	var ts = TweenMax.set;
-		ts(rect,{rotation:-90,transformOrigin:"50% 50%"})
-
-		var l = document.getElementById('mbLogo')
-		l.addEventListener('mouseover',function(){
-			showLogo()
-		})
+			
+			var hrEle = $(this).find(".clockHours");
+			var minEle = $(this).find(".clockMinutes");
+			
+			tt(hrEle, 1, {transformOrigin:"50% 50%", drawSVG: clockHours, overwrite:true});
+			tt(minEle, 1, {transformOrigin:"50% 50%", drawSVG: clockMinutes, overwrite:false});
+		});  
+	})();
 	
-		l.addEventListener('mouseout',function(){
-			hideLogo()
-		})
-
-		function showLogo(){
-			tt(rect,.4,{transformOrigin:"50% 50%", drawSVG:"0%",overwrite:true});
-			tt(bowenTXT,.4,{x:102, ease:Quad.easeIn, delay:0, overwrite:true});
-			tt(cgarryContainer,.4,{width:102, ease:Quad.easeIn, delay:0, overwrite:true});
-			tt(owenContainer,.4,{width:79, ease:Quad.easeOut, delay:.35, overwrite:true});
-		}
+	// Update clocks
 	
-		function hideLogo(){
-			tt(rect,.3,{transformOrigin:"50% 50%", delay:.2, drawSVG:"100%",overwrite:true});
-			tt(bowenTXT,.3,{x:0, ease:Quad.easeIn, delay:0, overwrite:true});
-			tt(cgarryContainer,.3,{width:0, ease:Quad.easeIn, delay:0, overwrite:true});
-			tt(owenContainer,.3,{width:0, ease:Quad.easeOut, delay:0, overwrite:true});
-		}
+	setInterval(function(){
+		$(".officeTile").each(function(index){
+			var timeOffset = parseInt($(this).data("timeOffset")),
+		  	d = new Date(),
+			utc = d.getTime() + (d.getTimezoneOffset() * 60000),
+			nd = new Date(utc + (3600000*timeOffset));
+
+			var ndHours = nd.getHours();
+			if(ndHours > 12) {
+				ndHours -= 12;
+			}
+			
+			var clockHours = ((ndHours/12) * 100) + "%";
+			var clockMinutes = (100 * (nd.getMinutes()/60))+"%";
+			
+			nd = nd.toLocaleString({hour: 'numeric', minute: 'numeric'}).replace(/:\d{2}\s/,' ').split(",")[1];
+  		  	
+			$(this).children("a").find("time").text(nd);
+			
+			var hrEle = $(this).find(".clockHours");
+			var minEle = $(this).find(".clockMinutes");
+			
+			tt(hrEle, 1, {transformOrigin:"50% 50%", drawSVG: clockHours, overwrite:true});
+			tt(minEle, 1, {transformOrigin:"50% 50%", drawSVG: clockMinutes, overwrite:false});
+		});
+	}, 60000);	
 });
