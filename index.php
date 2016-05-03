cea72a33f83826c2a19398cfc7ae012193580786
Jonathan Kelly <Jonathan.Kelly@mcgarrybowen.com>
2016-05-03 14:15:53 -0400

    turning on error reporting.

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<?php
+
+error_reporting(-1);
+ini_set('display_errors', 'On');
+
+?>
+
 <!DOCTYPE html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
