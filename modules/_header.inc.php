bdf248ca6059ce0d30d56869fda5cbc597baf7d5
Michael Gallay <michael.gallay@mcgarrybowen.com>
2016-05-20 15:27:47 -0400

    menu toggle animation

--- modules/_header.inc.php
+++ modules/_header.inc.php
@@ -2,31 +2,31 @@
 	<nav>
 		<a href="#Home" id="homeLogo">
 			<div id="mbLogo">
-				<svg version="1.1" baseProfile="tiny" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
-				     x="0px" y="0px"  xml:space="preserve">
-				<rect x="1" y="1" fill="none" stroke="#46ccc5" stroke-linecap="square" stroke-width="3" width="62" height="62" id="rect" />
-				</svg>
-
+				<svg version="1.1" baseProfile="tiny" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" xml:space="preserve"><rect x="1" y="1" fill="none" stroke="#46ccc5" stroke-linecap="square" stroke-width="3" width="62" height="62" id="rect" /></svg>
 				<div id="mcgarryTXT">
 					<div id="m"><?php addIcon('logo-m')?></div>
 					<div id="cgarryContainer">
 						<div id="cgarry"><?php addIcon('logo-cgarry')?></div>
 					</div>
 				</div>
-
 				<div id="bowenTXT">
 					<div id="b"><?php addIcon('logo-b')?></div>
 					<div id="owenContainer">
 						<div id="owen"><?php addIcon('logo-owen')?></div>
 					</div>
 				</div>
+			</div></a>
+		<div id="menuToggleHolder">
+			<div id="menuToggle">
+				
+					<div id="ham1"><div class="hamDot"></div></div>
+					<div id="ham2"><div class="hamDot"></div></div>
+					<div id="ham3"><div class="hamDot"></div></div>
+				
 			</div>
-		</a>
-		<div class="hamburger">
-			<div></div>
-			<div></div>
-			<div></div>
 		</div>
+		
+		
 		<ul class="menu">
 			<li>
 				<a href="#OurPeople" aria-label="">CULTURE</a>
@@ -43,5 +43,6 @@
 		</ul>
 		
 	</nav>
+	
 
 </header> 
