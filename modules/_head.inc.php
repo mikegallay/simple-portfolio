695d5d0d7a222c3774dbb0c24ffe8e6bed98e3ed
Michael Gallay <michael.gallay@mcgarrybowen.com>
2016-05-20 16:18:29 -0400

    pathing fixes

--- modules/_head.inc.php
+++ modules/_head.inc.php
@@ -24,9 +24,9 @@
 		
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, user-scalable=no"/>
 
-	<link rel="stylesheet" href="/assets/css/main.css">
+	<link rel="stylesheet" href="assets/css/main.css">
 
-	<script src="/assets/js/lib/modernizr-custom.js"></script>
+	<script src="assets/js/lib/modernizr-custom.js"></script>
 	<script>var appRoot='<?php echo $appRoot; ?>';</script>
 	
 	<script>
