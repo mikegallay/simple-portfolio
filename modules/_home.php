f0ea81edc5dd5f83972985b09c89e9e5955554d1
Michael Gallay <michael.gallay@ogilvy.com>
2019-03-12 14:35:30 -0400

    copy updates

--- modules/_home.php
+++ modules/_home.php
@@ -11,21 +11,21 @@
 		<section class="homeBio">
 			<p><strong>DEVELOPER, ILLUSTRATOR, TEAM-LEADER</strong></p>
 			<hr>
-			<p>With over 15 years in the digital industry in NYC, I am confident working independently or managing a team of developers in building cutting-edge digital experiences.<br/><br/>I am looking to become part of a group of like-minded individuals who are curious and excited about the future of technology. Currently available for freelance or full-time opportunities.<br/><br/><strong>Capabilities:</strong> Front-End Development, Wordpress, Banner Development, 3D Animation, After Effects, Illustration, Internet of Things, Virtual Reality
+			<p>With over 20 years in the digital industry in NYC, I am confident working independently or managing a team of developers in building cutting-edge digital experiences.<br/><br/>I am open to working with groups of like-minded individuals who are curious and excited about the future of technology.<br/><br/><strong>Capabilities:</strong> Technology Management, Front-End Development, Wordpress, 3D Animation, After Effects, Illustration, Internet of Things, Virtual Reality
 			</p>
 		</section>
 		<section id="homeTiles" class="fullBleed">
 			<div class="container">
 
 				<div class="columnWrapper">
-					<p><strong>SELECT WORK</strong></p>
+					<p><strong>RECENT PROJECTS</strong></p>
 					<hr>
 					<?php
 						$portfolioData = HTMLfromTemplateAndJSON("content/templates/portfolio-template.html", "portfolio-data");
 						echo $portfolioData;
 					?>
 					<br/>
-					<p><strong>DEVELOPMENT</strong></p>
+					<p><strong>FROM THE ARCHIVES</strong></p>
 					<hr>
 					<?php
 						$portfolioData = HTMLfromTemplateAndJSON("content/templates/portfolio-template.html", "dev-data");
